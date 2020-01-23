0691547931ff77353883322c72675f6e0c79691d
Michael Stiles <mstiles01@icloud.com>
2020-01-23 16:22:36 -0600

    Finished setup folders

---
+++ reactproject2/src/components/ExpenseList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Item from './ExpenseItem'
+export const ExpenseList = () => {
+    return (
+        <div>
+            Expense List!
+            <Item />
+        </div>
+    )
+}
